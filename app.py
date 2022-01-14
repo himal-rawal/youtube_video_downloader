11f00e5e90483346c46440dc74e209ae38f10b94
traitor <38320749+Traitor00@users.noreply.github.com>
2022-01-14 18:22:42 +0545

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ api = Api(app)
 
 
 class DownloadPlaylist(Resource):
-    def get(self, youtubeurl):
-        return {'data': main.downloadplaylist(youtubeurl)}
+    def get(self, playlistid):
+        return {'data': main.downloadplaylist(playlistid)}
 
 
-api.add_resource(DownloadPlaylist, '/download/<youtubeurl>') #I think giving url as a parameter in url is not acceptable so how can we use post request instead of get in this programs context
+api.add_resource(DownloadPlaylist, '/download/<playlistid>')
 
 if __name__ == '__main__':
     app.run()
