e749cfe61bd66bcaa5c246bd6ef9f7bc4ddbeccf
traitor <38320749+Traitor00@users.noreply.github.com>
2022-01-14 18:22:02 +0545

    Update main.py
    Changed p = Playlist("https://www.youtube.com/playlist?list="+playlistid)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytube import Playlist
-def downloadplaylist(youtubeurl):
+def downloadplaylist(playlistid):
     savepath = 'E:\youtubefolder'
-    p = Playlist(str(youtubeurl))#'https://www.youtube.com/playlist?list=PL6mx5D4YlhnTVgee_9pu6snpAQoB0viuD'
+    p = Playlist("https://www.youtube.com/playlist?list="+playlistid)#'https://www.youtube.com/playlist?list=PL6mx5D4YlhnTVgee_9pu6snpAQoB0viuD'
     print(f'Downloading: {p.title}')
     for audio in p.videos:
         print(audio.title)
